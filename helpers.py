076bc7b404498f6531bd969cf8588608b13794bb
Markkus Hipponen <markkus.hipponen@khk.ee>
2020-09-09 10:11:41 +0300

    lisatud kõikide elementide lugemine

--- helpers.py
+++ helpers.py
@@ -18,6 +18,14 @@ def lisa_elemendid(elementide_nimekiri):
     global elemendid
     elemendid = elementide_nimekiri
 
+# lisame ELEMENDID korraga, aga nii, et tagastame iga kord ühe elemendi
+
+def loe_elemendid():
+    global elemendid
+    loetud_elemendid = []
+    for element in elemendid:
+        loetud_elemendid.append(element)
+    return loetud_elemendid
 
 # loome main funktsiooni
 def main():
@@ -30,15 +38,14 @@ def main():
 
     # testime elementide lisamist
     lisa_elemendid(katse_elemendid)
-    print(elemendid)
+
 
     # testime üksikute elementide lisamist
     lisa_element("kohupiim", 0.90, 12)
-    print(elemendid)
-
     lisa_element("leib", 0.80, 5)
-    print(elemendid)
 
+    # testime elementide lugemist
+    print(loe_elemendid())
 
 # käivitamine
 if __name__ == "__main__":
